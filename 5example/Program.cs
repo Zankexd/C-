f3e142ce7fdb7045a17811be7000e184dce8b026
Oleksii <zankexd@icloud.com>
2022-04-30 12:08:48 +0200

    Masha

---
+++ 5example/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("please type your name");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "masha")
+{
+    Console.WriteLine ("Hi Maria");
+}
+else
+{
+    Console.Write (name + ", hi") ;
+
+   
+}
