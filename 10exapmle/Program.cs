fe2454e237c133710c9daa5f86de0f2665f34c43
Oleksii <zankexd@icloud.com>
2022-04-30 17:32:27 +0200

    nedela

---
+++ 10exapmle/Program.cs
@@ -0,0 +1,11 @@
+﻿string week = Console.ReadLine();
+int week2 = int.Parse(week);
+
+if(week2==1) Console.Write("Pon");
+if(week2==2) Console.Write("Wtr");
+if(week2==3) Console.Write("srd");
+if(week2==4) Console.Write("cht");
+if(week2==5) Console.Write("frid");
+if(week2==6) Console.Write("subb");
+if(week2==7) Console.Write("nedela");
+if(week2>7) Console.Write("Maksimym 7");
