c4b3c8dae10191095ad413b5cd404c152e219621
Oleksii <zankexd@icloud.com>
2022-04-30 11:57:10 +0200

    random

---
+++ 4example/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random ().Next(-400 , 10);
+int numberB = new Random ().Next(-100 , 10);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
