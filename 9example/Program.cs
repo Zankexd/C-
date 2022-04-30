740d0d37b9797e09761eb4880d23eb3a9c7325fa
Oleksii <zankexd@icloud.com>
2022-04-30 15:33:03 +0200

    ez

---
+++ 9example/Program.cs
@@ -0,0 +1,17 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+int a1 = int.Parse(a);
+int b1 = int.Parse(b);
+if (a1 * a1 == b1)
+{
+    Console.WriteLine("Pravda1");
+}
+if (b1 * b1 == a1)
+{
+    Console.WriteLine("Pravda2");
+}
+else
+{
+    Console.WriteLine("Nety pary");
+
+}
